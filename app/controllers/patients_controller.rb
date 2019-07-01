9ea176b5093f3fdb5cf8d42f45155595704d7de7
mochihara <hogahuga>
2019-07-01 21:56:13 +0900

    とりあえず療養者登録画面

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,6 +1,6 @@
 class PatientsController < ApplicationController
   def index
-    @patients = Patient.all
+    @patients = Patient.all()
   end
 
   def show
@@ -11,5 +11,19 @@ class PatientsController < ApplicationController
     @patient = Patient.new()
   end
 
-  
+  def create
+    @patient = Patient.new()
+    @patient.name = params[:name]
+    @patient.name_kana = params[:name_kana]
+    @patient.age = params[:age]
+    @patient.sex = params[:sex]
+    if @patient.save()
+      flash[:notice] = "新しく療養者を登録しました"
+      redirect_to("/patients")
+    else
+      render("patients/new")
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # トップページ
   get '/' => 'home#top'
 
-  # 療養者詳細
+  # 療養者新規登録ページ
+  get 'patients/new' => 'patients#new'
+  # 療養者新規登録
+  post 'patients/create' => 'patients#create'
+  # 療養者詳細ページ
   get 'patients/:id' => 'patients#show'
   # 療養者一覧
   get 'patients' => 'patients#index'
