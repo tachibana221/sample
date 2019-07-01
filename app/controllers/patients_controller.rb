c71202f5af06dcafacc60941ec978b1a70ccd686
mochihara <hogahuga>
2019-07-01 21:06:22 +0900

    患者詳細ページ実装

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,4 +2,14 @@ class PatientsController < ApplicationController
   def index
     @patients = Patient.all
   end
+
+  def show
+    @patient = Patient.find_by(id: params[:id])
+  end
+
+  def new
+    @patient = Patient.new()
+  end
+
+  
 end
