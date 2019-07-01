167dfd96b93a3f16b104606d3f0785e8b055e241
mochihara <hogahuga>
2019-07-01 23:26:34 +0900

    編集ページ作成

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -11,6 +11,10 @@ class PatientsController < ApplicationController
     @patient = Patient.new()
   end
 
+  def edit
+    @patient = Patient.find_by(id: params[:id])
+  end
+
   def create
     @patient = Patient.new()
     @patient.name = params[:name]
@@ -22,6 +26,20 @@ class PatientsController < ApplicationController
       redirect_to("/patients")
     else
       render("patients/new")
+    end
+  end
+
+  def update
+    @patient = Patient.find_by(id: params[:id])
+    @patient.name = params[:name]
+    @patient.name_kana = params[:name_kana]
+    @patient.age = params[:age]
+    @patient.sex = params[:sex]
+    if @patient.save()
+      flash[:notice] = "登録情報を更新しました"
+      redirect_to("/patients")
+    else
+      render("patients/edit")
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
   post 'patients/create' => 'patients#create'
   # 療養者詳細ページ
   get 'patients/:id' => 'patients#show'
+  # 療養者情報編集ページ
+  get 'patients/:id/edit' => 'patients#edit'
+  # 療養者情報更新
+  get 'patients/:id/update' => 'patients#update'
   # 療養者一覧
   get 'patients' => 'patients#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
