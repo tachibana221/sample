c86bd9945960c7ccd0fe56e9b6860fdf0d95b09c
mochihara <hogahuga>
2019-07-02 11:21:28 +0900

    patientのCRUDそれぞれのviewとcontroller実装

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,26 +1,34 @@
 class PatientsController < ApplicationController
+
+  # 一覧ページ
   def index
     @patients = Patient.all()
   end
 
+  # 詳細ページ
   def show
     @patient = Patient.find_by(id: params[:id])
   end
 
+  # 新規作成ページ
   def new
     @patient = Patient.new()
   end
 
+  # 編集ページ
   def edit
     @patient = Patient.find_by(id: params[:id])
   end
 
+  # 新規作成メソッド
   def create
     @patient = Patient.new()
-    @patient.name = params[:name]
-    @patient.name_kana = params[:name_kana]
-    @patient.age = params[:age]
-    @patient.sex = params[:sex]
+    # フォームの値取り出し
+    form_params = params[:patient]
+    @patient.name = form_params[:name]
+    @patient.name_kana = form_params[:name_kana]
+    @patient.age = form_params[:age]
+    @patient.sex = form_params[:sex]
     if @patient.save()
       flash[:notice] = "新しく療養者を登録しました"
       redirect_to("/patients")
@@ -29,18 +37,28 @@ class PatientsController < ApplicationController
     end
   end
 
+  # 更新メソッド
   def update
     @patient = Patient.find_by(id: params[:id])
-    @patient.name = params[:name]
-    @patient.name_kana = params[:name_kana]
-    @patient.age = params[:age]
-    @patient.sex = params[:sex]
+    # フォームの値取り出し
+    form_params = params[:patient]
+    @patient.name = form_params[:name]
+    @patient.name_kana = form_params[:name_kana]
+    @patient.age = form_params[:age]
+    @patient.sex = form_params[:sex]
     if @patient.save()
       flash[:notice] = "登録情報を更新しました"
-      redirect_to("/patients")
+      redirect_to("/patients/#{params[:id]}")
     else
-      render("patients/edit")
+      render("patients/#{params[:id]}/edit")
     end
+  end
+
+  # 削除メソッド
+  def destroy
+    @patient = Patient.find_by(id: params[:id])
+    @patient.destroy()
+    redirect_to("/patients")
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,11 @@ Rails.application.routes.draw do
   # トップページ
   get '/' => 'home#top'
 
-  # 療養者新規登録ページ
-  get 'patients/new' => 'patients#new'
-  # 療養者新規登録
-  post 'patients/create' => 'patients#create'
-  # 療養者詳細ページ
-  get 'patients/:id' => 'patients#show'
-  # 療養者情報編集ページ
-  get 'patients/:id/edit' => 'patients#edit'
-  # 療養者情報更新
-  get 'patients/:id/update' => 'patients#update'
-  # 療養者一覧
-  get 'patients' => 'patients#index'
+  # これでそれぞれにrouteingを書かなくてもいい感じにしてくれる
+  # まじでon railsってかんじ
+  # https://guides.rubyonrails.org/routing.html#crud-verbs-and-actions
+  resources :patients
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
