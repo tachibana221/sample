af705423d4861d556540524ec758f0d99cf74bc2
ryo mochihara <mochi.ryo0616@gmail.com>
2019-07-02 16:18:38 +0900

    nurseモデル

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # まじでon railsってかんじ
   # https://guides.rubyonrails.org/routing.html#crud-verbs-and-actions
   resources :patients
+  resources :nurses
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
