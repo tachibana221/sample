16af8b0e056553e9743be361566aa260e7f1bcd6
ryo mochihara <mochi.ryo0616@gmail.com>
2019-07-11 13:20:12 +0900

    お絵かき

--- app/assets/javascripts/bedsores.js
+++ app/assets/javascripts/bedsores.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function drwaing(){
+  console.log("aaa");
+  console.log(document.querySelector('#mySketcher'));
+  var sketcher = atrament('#mySketcher');
+  //or use a more object-oriented style
+}
+
+window.onload = drwaing
